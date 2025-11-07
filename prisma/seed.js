caaf6401b9958ffd676f79de67177a72529bd812
Dilukshan <65407969+dilukshann7@users.noreply.github.com>
2025-11-07 22:59:36 +0530

    Add Prisma client and seed script
    Introduced @prisma/client as a dependency and added a seed script to create a default user. Refactored code to remove Prisma client initialization from src/index.js and moved related logic to prisma/seed.js. Updated package.json to set module type.

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,9 +1,18 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../src/generated/prisma/client.ts";
+import "dotenv/config";
 
 const prisma = new PrismaClient();
 
 async function main(params) {
   //Prisma Queries can be added here if needed
+  const user = await prisma.user.create({
+    data: {
+      name: "John Doe",
+      email: "john@email.com",
+      password: "password",
+      role: "manager",
+    },
+  });
 }
 
 main()
--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
 import "./css/style.css";
 import { router } from "../src/js/middleware/router.js";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-async function main(params) {
-  //Prisma Queries can be added here if needed
-}
 
 // Initialize the router
 window.addEventListener("DOMContentLoaded", router);
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
