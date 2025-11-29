9c4f34629391a1862bacfecdee4ec662060efd47
Melisha Devaraj <shenalimelissa@gmail.com>
2025-11-29 15:33:12 +0530

    feat(client): Implement Task model CRUD operations

--- src/js/models/Task.js
+++ src/js/models/Task.js
@@ -22,4 +22,22 @@ export class Task {
 
     return axios.get(apiURL);
   }
+
+  static async create(task) {
+    const apiURL = "http://localhost:3000/api/tasks";
+
+    return axios.post(apiURL, task);
+  }
+
+  static async update(id, task) {
+    const apiURL = `http://localhost:3000/api/tasks/${id}`;
+
+    return axios.put(apiURL, task);
+  }
+
+  static async delete(id) {
+    const apiURL = `http://localhost:3000/api/tasks/${id}`;
+
+    return axios.delete(apiURL);
+  }
 }
