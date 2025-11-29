ebc7df13fabd97d8391edaa67ff5daeb020939b1
Melisha Devaraj <shenalimelissa@gmail.com>
2025-11-29 15:33:11 +0530

    feat(client): Implement Product model CRUD operations

--- src/js/models/Product.js
+++ src/js/models/Product.js
@@ -26,4 +26,22 @@ export class Product {
 
     return axios.get(apiURL);
   }
+
+  static async create(product) {
+    const apiURL = "http://localhost:3000/api/products";
+
+    return axios.post(apiURL, product);
+  }
+
+  static async update(id, product) {
+    const apiURL = `http://localhost:3000/api/products/${id}`;
+
+    return axios.put(apiURL, product);
+  }
+
+  static async delete(id) {
+    const apiURL = `http://localhost:3000/api/products/${id}`;
+
+    return axios.delete(apiURL);
+  }
 }
