4c901fceddcfb3ee3444b9fa4e0e497b6f175a06
Melisha Devaraj <shenalimelissa@gmail.com>
2025-11-29 17:44:04 +0530

    Refactor: Remove redundant model constructors

--- src/js/models/SalesInvoice.js
+++ src/js/models/SalesInvoice.js
@@ -1,20 +1,5 @@
 import axios from "axios";
 export class SalesInvoice {
-  constructor(data) {
-    this.id = data.id;
-    this.invoiceNumber = data.invoiceNumber;
-    this.customerId = data.customerId;
-    this.date = data.date;
-    this.paymentMethod = data.paymentMethod;
-    this.items = data.items; // Array of items
-    this.subtotal = data.subtotal;
-    this.collectedAmount = data.collectedAmount;
-    this.collectedAt = data.collectedAt;
-    this.status = data.status;
-    this.createdAt = data.createdAt;
-    this.updatedAt = data.updatedAt;
-  }
-
   static async getAll() {
     const apiURL = "http://localhost:3000/api/sales-invoices";
     return axios.get(apiURL);
--- src/js/models/SalesOrder.js
+++ src/js/models/SalesOrder.js
@@ -1,26 +1,6 @@
 import axios from "axios";
 
-// SalesOrder Class
 export class SalesOrder {
-  constructor(data) {
-    this.id = data.id;
-    this.orderNumber = data.orderNumber;
-    this.customerId = data.customerId;
-    this.customerName = data.customerName;
-    this.salesmanId = data.salesmanId;
-    this.orderDate = data.orderDate;
-    this.subtotal = data.subtotal;
-    this.tax = data.tax;
-    this.discount = data.discount;
-    this.totalAmount = data.totalAmount;
-    this.status = data.status;
-    this.paymentStatus = data.paymentStatus;
-    this.items = data.items || [];
-    this.notes = data.notes;
-    this.createdAt = data.createdAt;
-    this.updatedAt = data.updatedAt;
-  }
-
   static async getAll(filters) {
     const apiURL = "http://localhost:3000/api/sales-orders";
 
