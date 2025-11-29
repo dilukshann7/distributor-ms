27cf899324fe93ded90c1b6f0a2a5da36e93ecf8
Melisha Devaraj <shenalimelissa@gmail.com>
2025-11-29 21:07:33 +0530

    Extend Driver from User and add CRUD methods
    Driver now extends the User class and includes static methods for creating, updating, and deleting drivers via the API, in addition to fetching all drivers and finding by ID.

--- src/js/models/Drivers.js
+++ src/js/models/Drivers.js
@@ -1,12 +1,29 @@
 import axios from "axios";
+import { User } from "./User";
 
-export class Driver {
+export class Driver extends User {
   static async getAll() {
     const apiURL = "http://localhost:3000/api/drivers";
     return axios.get(apiURL);
   }
+
   static async findById(id) {
     const apiURL = `http://localhost:3000/api/drivers/${id}`;
     return axios.get(apiURL);
+  }
+
+  static async create(driverData) {
+    const apiURL = "http://localhost:3000/api/drivers";
+    return axios.post(apiURL, driverData);
+  }
+
+  static async update(id, driverData) {
+    const apiURL = `http://localhost:3000/api/drivers/${id}`;
+    return axios.put(apiURL, driverData);
+  }
+
+  static async delete(id) {
+    const apiURL = `http://localhost:3000/api/drivers/${id}`;
+    return axios.delete(apiURL);
   }
 }
