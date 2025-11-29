dc712d87a3a29b80e0a504ce2dd2cb40c3d71f2c
Melisha Devaraj <shenalimelissa@gmail.com>
2025-11-29 22:51:42 +0530

    Automate delivery creation on order approval
    Order approval now automatically creates a delivery record and links it to the sales order. Refactored Delivery model to add a static create method and removed unused constructor. Also removed driver info rendering and related code from OrderAuthorization for simplification.

--- src/js/models/Delivery.js
+++ src/js/models/Delivery.js
@@ -1,27 +1,15 @@
 import axios from "axios";
 
-// Delivery Class
 export class Delivery {
-  constructor(data) {
-    this.id = data.id;
-    this.deliveryNumber = data.deliveryNumber;
-    this.orderId = data.orderId;
-    this.driverId = data.driverId;
-    this.vehicleId = data.vehicleId;
-    this.deliveryAddress = data.deliveryAddress;
-    this.scheduledDate = data.scheduledDate;
-    this.deliveredDate = data.deliveredDate;
-    this.estimatedTime = data.estimatedTime;
-    this.status = data.status;
-    this.signature = data.signature;
-    this.proofOfDelivery = data.proofOfDelivery;
-    this.notes = data.notes;
-    this.createdAt = data.createdAt;
-    this.updatedAt = data.updatedAt;
-  }
   static async getAll(filters) {
     const apiURL = "http://localhost:3000/api/deliveries";
 
     return axios.get(apiURL);
+  }
+
+  static async create(data) {
+    const apiURL = "http://localhost:3000/api/deliveries";
+
+    return axios.post(apiURL, data);
   }
 }
