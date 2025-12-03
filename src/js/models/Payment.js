28d29ce42db4b25254f31441f2fef5f9e843d87c
Pabodhini Tharaka Perera <ahasya2004@gmail.com>
2025-12-03 22:55:17 +0530

    Remove update and delete methods from Payment model
    The update and delete static methods have been removed from the Payment model, leaving only the create method. This simplifies the model and may reflect a change in API usage or business requirements.

--- src/js/models/Payment.js
+++ src/js/models/Payment.js
@@ -12,16 +12,4 @@ export class Payment {
 
     return axios.post(apiURL, payment);
   }
-
-  static async update(id, payment) {
-    const apiURL = `http://localhost:3000/api/payments/${id}`;
-
-    return axios.put(apiURL, payment);
-  }
-
-  static async delete(id) {
-    const apiURL = `http://localhost:3000/api/payments/${id}`;
-
-    return axios.delete(apiURL);
-  }
 }
