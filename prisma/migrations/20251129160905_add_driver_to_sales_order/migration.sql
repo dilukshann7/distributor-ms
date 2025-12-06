33120bb793e86d623c9d74e2840a4e41fbfe9d70
Anjana Dulan Nimsara <dulannimsara453@gmail.com>
2025-12-06 20:37:08 +0530

    Consolidate and recreate Prisma migrations
    Removed all previous migration files and added a new consolidated migration. This simplifies the migration history and reflects the current database schema in a single migration file.

--- prisma/migrations/20251129160905_add_driver_to_sales_order/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "SalesOrder" ADD COLUMN     "driverId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "SalesOrder" ADD CONSTRAINT "SalesOrder_driverId_fkey" FOREIGN KEY ("driverId") REFERENCES "Driver"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- prisma/migrations/20251203181638_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `pendingPayment` on the `SalesOrder` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "SalesOrder" DROP COLUMN "pendingPayment";
