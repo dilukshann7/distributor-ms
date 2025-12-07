e3443206e67736295114e6d89f4b65df1f10b0e5
Dilukshan Niranjan <dilukshann7@gmail.com>
2025-12-07 19:45:42 +0530

    Remove FinancialReport model
    Deleted FinancialReport.js as it is no longer needed. All related API calls should be refactored to use alternative methods.

--- src/js/models/FinancialReport.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export class FinancialReport {
-  static async getMonthlyOverview() {
-    const apiURL = "/api/financial-overview";
-    return axios.get(apiURL);
-  }
-}
